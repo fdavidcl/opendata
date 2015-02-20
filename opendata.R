50c9ddf1329f95e79f42215f0c06dabf89c299d8
David Charte <fdavidcl@outlook.com>
2015-02-20 11:22:27 +0100

    Parsing decimal commas

--- opendata.R
+++ opendata.R
@@ -1,7 +1,19 @@
+# install.packages(httr)
 library(httr)
 
 get_data <- function(url) {
   filename <- tempfile()
   write.csv(content(GET(url)), filename)
   read.csv(filename)
+}
+
+parse_comma <- function(col) {
+  as.numeric(sub(",", ".", col, fixed = TRUE))
+}
+
+to_numeric <- function(col) {
+  if (is.factor(col))
+    col <- as.character(col)
+
+  parse_comma(col)
 }
